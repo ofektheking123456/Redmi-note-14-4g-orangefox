26fbce302cc58ca807ad4d5f38d647906cd5174c
ofektheking123456 <ofek2oo2ooo@gmail.com>
2026-08-20 12:38:23 +0300

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -9,6 +9,8 @@ TARGET_2ND_ARCH := arm
 TARGET_2ND_ARCH_VARIANT := armv7-a-neon
 TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_SUPPORTS_64_BIT_APPS := true
+TARGET_SUPPORTS_32_BIT_APPS := true
 
 # Bootloader & Platform
 TARGET_BOOTLOADER_BOARD_NAME := tanzanite
