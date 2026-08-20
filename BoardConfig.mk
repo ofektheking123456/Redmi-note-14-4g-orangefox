a190843daa38ae9d3b466453635c1e7a26b6fe30
ofektheking123456 <ofek2oo2ooo@gmail.com>
2026-08-20 12:22:35 +0300

    Update BoardConfig.mk with architecture settings
    Added architecture and bootloader configurations for the board.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,6 +1,24 @@
 LOCAL_PATH := device/xiaomi/tanzanite
-TARGET_BOARD_PLATFORM := mt6789
+
+# Architecture
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 :=
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv7-a-neon
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+
+# Bootloader & Platform
+TARGET_BOOTLOADER_BOARD_NAME := tanzanite
+TARGET_BOARD_PLATFORM := mt6789
+TARGET_NO_BOOTLOADER := true
+
+# Storage & System
 BOARD_HAS_NO_REAL_SDCARD := true
 BOARD_BUILD_SYSTEM_ROOT_IMAGE := false
+
+# OrangeFox Build Flags
+ALLOW_MISSING_DEPENDENCIES := true
+TW_THEME := portrait_hdpi
