3977bdbca61ad4f3d9ad4559c4de811f8135aeb3
ofektheking123456 <ofek2oo2ooo@gmail.com>
2026-08-20 12:40:05 +0300

    Inherit 64-bit architecture support
    Added support for 64-bit architecture by inheriting core_64_bit.mk.

--- omni_tanzanite.mk
+++ omni_tanzanite.mk
@@ -1,3 +1,6 @@
+# Inherit 64-bit architecture support
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+
 # Inherit device configuration
 $(call inherit-product, device/xiaomi/tanzanite/device.mk)
 
