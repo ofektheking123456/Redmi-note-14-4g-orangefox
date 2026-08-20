51d8e51343887917d69338f82517f8139116c110
ofektheking123456 <ofek2oo2ooo@gmail.com>
2026-08-20 12:08:23 +0300

    Update omni_tanzanite.mk

--- omni_tanzanite.mk
+++ omni_tanzanite.mk
@@ -1,15 +1,10 @@
-# Inherit from minimum generic Android products
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
-
-# Inherit from OrangeFox / TWRP setup
-$(call inherit-product, vendor/recovery/config/common.mk)
-
 # Inherit device configuration
 $(call inherit-product, device/xiaomi/tanzanite/device.mk)
 
+# OrangeFox Build Vars
+FOX_BUILD_DEVICE := tanzanite
 PRODUCT_NAME := omni_tanzanite
 PRODUCT_DEVICE := tanzanite
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 14 4G
-$(call inherit-product, device/xiaomi/tanzanite/device.mk)
-
+PRODUCT_MANUFACTURER := Xiaomi
