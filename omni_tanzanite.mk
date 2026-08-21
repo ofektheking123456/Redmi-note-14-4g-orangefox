fb8f720eabfca5d6251a5c6ac2e22b2a43e9243a
ofektheking123456 <ofek2oo2ooo@gmail.com>
2026-08-21 14:27:06 +0300

    Add OrangeFox configuration inheritance

--- omni_tanzanite.mk
+++ omni_tanzanite.mk
@@ -1,6 +1,9 @@
 # Inherit 64-bit architecture support
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 
+# Inherit OrangeFox configuration
+$(call inherit-product, vendor/fox/config/common.mk)
+
 # Inherit device configuration
 $(call inherit-product, device/xiaomi/tanzanite/device.mk)
 
